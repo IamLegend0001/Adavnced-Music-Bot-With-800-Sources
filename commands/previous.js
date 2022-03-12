f540a8204a181d63e157c155d8fcc6582f7709b5
LegendCodez0001 <100712216+IamLegend0001@users.noreply.github.com>
2022-03-12 17:43:41 +0530

    Update previous.js

--- commands/previous.js
+++ commands/previous.js
@@ -8,3 +8,11 @@ module.exports = {
       message.channel.send(`${client.emotes.success} | Now playing:\n${song.name}`)
     }
   }
+
+//Made By Legend Codez
+
+//Must Give Credits While Using - https://dsc.gg/shades-dev |https://www.youtube.com/channel/UCoo2H78DlUGKRoShdOeyObQ,
+
+//Made By Shades Development 
+
+//12/3/22 | 5:33pm
