3a3eb3f44e4e56a4578cac9f6b4806764d641b13
LegendCodez0001 <100712216+IamLegend0001@users.noreply.github.com>
2022-03-12 17:38:37 +0530

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -2,5 +2,6 @@ module.exports = {
     name: 'leave',
     run: async (client, message) => {
       client.distube.voices.leave(message)
+        message.channel.send(`**Left Your Voice Channel!**`)
     }
   }
