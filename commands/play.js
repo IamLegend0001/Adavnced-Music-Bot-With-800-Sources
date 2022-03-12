8c4f5906691f9c76f2a142a9b2471a7d09276aa9
LegendCodez0001 <100712216+IamLegend0001@users.noreply.github.com>
2022-03-12 17:43:19 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -20,3 +20,11 @@ module.exports = {
       })
     }
   }
+
+//Made By Legend Codez
+
+//Must Give Credits While Using - https://dsc.gg/shades-dev |https://www.youtube.com/channel/UCoo2H78DlUGKRoShdOeyObQ,
+
+//Made By Shades Development 
+
+//12/3/22 | 5:33pm
