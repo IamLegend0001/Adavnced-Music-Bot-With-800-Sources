371e5ac55afd2e71c6cc3a5bd617196ad59e5962
LegendCodez0001 <100712216+IamLegend0001@users.noreply.github.com>
2022-03-12 17:33:24 +0530

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -10,7 +10,7 @@ module.exports = {
           new Discord.MessageEmbed()
           .setFooter(`Requested By ${message.author.tag}`)
           .setColor("#ff0000")
-          .setDescription(`${client.emotes.error} | There is nothing in the queue right now!`)
+          .setDescription(`**${client.emotes.error} | Queue Is Empty**`)
           .setThumbnail(`${client.user.displayAvatarURL ({dynamic: true})}`)
         ]
       })
@@ -18,3 +18,11 @@ module.exports = {
       message.channel.send(`${client.emotes.success} | AutoPlay: \`${autoplay ? 'On' : 'Off'}\``)
     }
   }
+
+//Made By Legend Codez
+
+Must Give Credits While Using - https://dsc.gg/shades-dev |https://www.youtube.com/channel/UCoo2H78DlUGKRoShdOeyObQ,
+
+Made By Shades Development 
+
+12/3/22 | 5:33pm
